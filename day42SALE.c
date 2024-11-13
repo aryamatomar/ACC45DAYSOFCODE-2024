a533b8efa1198261c18583ca5c9a399fbba51189
aryamatomar <TOMARARYAMA2006@GMAIL.COM>
2024-11-13 22:22:37 +0530

    Add files via upload

---
+++ day42SALE.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T); 
+
+    while(T--) {
+        int A, B, C;
+        scanf("%d %d %d", &A, &B, &C);
+
+        
+        int total_price = A + B + C;
+        int min_price = A;
+
+        
+        if (B < min_price) min_price = B;
+        if (C < min_price) min_price = C;
+
+      
+        int amount_to_pay = total_price - min_price;
+
+        printf("%d\n", amount_to_pay);
+    }
+
+    return 0;
+}
